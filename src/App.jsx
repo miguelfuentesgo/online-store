b1934de478074706723e7cdaba635d848aaa6b75
miguelit0 <meduardofuentesc@gmail.com>
2024-05-14 13:57:08 -0500

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="text-3xl font-bold underline">
+      Hola mundo de react, im alive
+    </div>
+  )
+}
+
+export default App
